84ee2dec4ac9aad783163e539449898cd007a4a7
dylanbeder <dylan.beder@duke.edu>
2018-06-14 18:35:53 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,10 +1,18 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {:patriarch
-      }, 
-    :capulet => {
-      #your key/value pairs here!
-      }}
+   :montague => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+   },
+   :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+   }
+}
 
   
 end
